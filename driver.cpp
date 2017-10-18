4f21ad01fc118ae2a861ba4131aa5bd2d2774c3d
syzko <bensingersteven@outlook.com>
2017-10-18 19:03:47 -0400

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -19,17 +19,21 @@
 #include"login.h"
 
 using namespace std;
-const int NUM_OF_ATTRIBUTES = 5;
 
+const int NUM_OF_ATTRIBUTES = 5;//constant for userTable size
+
+//loads a list of possible users from a text file into a 2D vector 
 vector<vector<string>> userTable(string file)
 {
 
 
-	int lineCount = 0;
-	string line;
-	ifstream inFile;
+	int lineCount = 0; //variable used to set userTable size 
+	string line; //line to be counted
+	ifstream inFile;//variable to open desired file
 
 	inFile.open(file);
+
+	//gets number of lines contained by the inFile
 	while (!inFile.eof())
 	{
 		getline(inFile, line);
@@ -37,9 +41,11 @@ vector<vector<string>> userTable(string file)
 	}
 	inFile.close();
 
-	vector<vector<string>> userArray(lineCount, vector<string>(NUM_OF_ATTRIBUTES));
+	vector<vector<string>> userArray(lineCount, vector<string>(NUM_OF_ATTRIBUTES));//declaring userTable
 
 	inFile.open(file);
+	
+	//fills the 2D vector with all users and their attributes
 	for (int i = 0; i < lineCount; i++)
 	{
 		for (int j = 0; j < NUM_OF_ATTRIBUTES; j++)
@@ -49,23 +55,59 @@ vector<vector<string>> userTable(string file)
 	}
 
 	return userArray;
-}
+}//end userTable
+
+ //loads a list of possible departments from a text file into a vector 
+vector<string> deptTable(string file)
+{
+
+	int lineCount = 0; //variable used to set deptTable size 
+	string line; //line to be counted
+	ifstream inFile;//variable to open desired file
+
+	inFile.open(file);
+
+	//gets number of lines contained by the inFile
+	while (!inFile.eof())
+	{
+		getline(inFile, line);
+		lineCount++;
+	}
+	inFile.close();
+
+	vector<string> deptArray(lineCount);//declaring deptTable
+
+	inFile.open(file);
+
+	//fills the vector with the departments
+	for (int i = 0; i < lineCount; i++)
+	{
+		inFile >> deptArray[i];
+	}
+	
+
+	return deptArray;
+}//end deptTable
 
 
 int main()
 {
-restartAll:
+restartAll: 
 
 	menu myLoginMenu("loginMenu.txt");
 	login myLogin;
 	int indexSelection;
 	vector<vector<string>> user = userTable("credentials.txt");
+	vector<string> departments = deptTable("deptList.txt");
+	
+	employeeType curEmployee;
 	string department;
 	string username;
 	string password;
+	
 	bool exitSwitch1 = false;
 	bool exitSwitch2 = false;
-	employeeType curEmployee;
+	
 
 	while (!exitSwitch1)
 	{
@@ -107,18 +149,44 @@ restartAll:
 		case 1:
 			exitSwitch1 = true;
 			exitSwitch2 = true;
+			system("cls");
 			break;
 		}
 	}
 
 	while (!exitSwitch2)
 	{
-		system("cls");
-		system("cls");
-		cout << curEmployee.getFirstName() << " " << curEmployee.getLastName() << " "
-			<< curEmployee.getDepartment() << endl; 
-		exitSwitch2 = true;
+		for (int i = 0; i < departments.size(); i++)
+		{
+			if (curEmployee.getDepartment() == departments[i])
+				indexSelection = i;
+		}
 		
+		switch (indexSelection)
+		{
+		case 0:
+			cout << "Admin" << endl;
+			break;
+		case 1:
+			cout << "HR" << endl;
+			break;
+		case 2:
+			cout << "Sales" << endl;
+			break;
+		case 3:
+			cout << "Purchasing" << endl;
+			break;
+		case 4:
+			cout << "Accounting" << endl;
+			break;
+		case 5:
+			cout << "Warehouse" << endl;
+			break;
+			
+
+		}
+		
+		exitSwitch2 = true;
 	}
 
 	cout << endl << endl;
